0d69f1dbbcd6ecd2b346c19e48e1b875981b8461
NekoYo1 <kostyafear203@gmail.com>
2022-11-26 09:03:23 +1000

    Добавьте файлы проекта.

---
+++ SayGoodByeMorozov/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayGoodByeMorozov
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Goodbye World!");
+        }
+    }
+}
---
+++ SayGoodbyeWorldTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SayGoodbyeWorldTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SayGoodbyeWorldTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af8eff27-1d83-4bbe-9a32-95c3d06fce34")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
